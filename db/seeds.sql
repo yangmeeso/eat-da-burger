e70236790c44cd9c360c25d8ab26deaa2c21486f
yangmeeso <yangmeeso@gmail.com>
2018-01-24 18:11:03 -0800

    Initial files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Triple Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Western Burger');
+INSERT INTO burgers (burger_name) VALUES ('Egg Avocado Bacon Burger');
